bf921123cbf40fea3d087d97c73f203cf741b8a1
armanpinachyan <pinachyan99@mail.ru>
2023-08-31 18:08:02 +0400

    18;07

--- js/script.js
+++ js/script.js
@@ -4,3 +4,24 @@ const {
     active: 'active'
 }
 
+const menuItemMobile = document.querySelector('.menu-item-mobile');
+const mobileMenBoard = document.querySelector('.mobile-menu-board');
+
+mobileMenBoard.addEventListener('click', function (){
+    if(this.classList.contains(active)){
+        this.classList.remove(active);
+        menuItemMobile.classList.remove(active);
+        document.body.style.overflow = null;
+    } else {
+        this.classList.add(active);
+        menuItemMobile.classList.add(active);
+        document.body.style.overflow = 'hidden';
+    }
+})
+
+const menu_item_mobile_teg = document.querySelector('.menu-item-mobile-teg')
+const menu_ItemMobile = document.querySelector('.menu-item-mobile');
+
+menu_item_mobile_teg.addEventListener('click', function (){
+    menu_ItemMobile.classList.remove(active);
+})
